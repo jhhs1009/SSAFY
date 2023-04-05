a151d82eaffffd1fb970d7904c55d7266735814f
jhhs1009 <c6731105@gmail.com>
2023-04-05 15:06:34 +0900

    DB 실습

--- April/Database_/0405/dbproblem_1-1/ddl_practice.sql
+++ April/Database_/0405/dbproblem_1-1/ddl_practice.sql
@@ -7,4 +7,7 @@ CREATE TABLE users (
   balance INTEGER NOT NULL
 );
 
-SELECT first_name, age FROM users ORDER BY first_name asc, age desc;
+-- 나이가 어린 순서로 조회하는 관리자 페이지를 구현하고자 함
+-- 페이지 당 출력되는 데이터는 20개로 제한했을 때, 3번째 페이지를 조회하는 쿼리문을 작성
+SELECT first_name, age FROM users
+ORDER BY age LIMIT 40 OFFSET 20;
