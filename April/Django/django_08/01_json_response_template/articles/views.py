820d69ba34ba146271fb41c64df46e8a3da0757a
fpqaut <fpqaut@naver.com>
2023-04-13 17:54:11 +0900

    0413 장고

---
+++ April/Django/django_08/01_json_response_template/articles/views.py
@@ -0,0 +1,22 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.shortcuts import render
+from django.http.response import JsonResponse, HttpResponse
+from django.core import serializers
+from .models import Article
+from .serializers import ArticleSerializer, ArticleListSerializer
+
+
+# Create your views here.
+@api_view(['GET'])
+def article_list(request):
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def article_detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    serializer = ArticleListSerializer(article)
+    return Response(serializer.data)
