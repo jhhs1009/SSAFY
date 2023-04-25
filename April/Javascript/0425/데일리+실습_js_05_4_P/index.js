5debd41bdcf96fe434d9eda03a2c10bb2b982a8c
jhhs1009 <c6731105@gmail.com>
2023-04-25 23:48:30 +0900

    0425 js url 받아왔음, 앨범 리스트 뽑아야함

--- April/Javascript/0425/데일리+실습_js_05_4_P/index.js
+++ April/Javascript/0425/데일리+실습_js_05_4_P/index.js
@@ -2,16 +2,29 @@
   아래에 코드를 작성해주세요.
 */
 
-const keyword = document.querySelector('input')
+const input = document.querySelector('input')
 const search = document.querySelector('button')
 
 function fetchAlbums(page, limit) {
   page = 1
   limit = 10
-  alert('확인!')
+  // alert('확인!')
+  const keyword = document.getElementsByClassName('search-box__input')[0].value
+  console.log(keyword)
+
+  const baseurl = 'https://ws.audioscrobbler.com/2.0/?method=album.search&album='
+  const api ='&api_key=ca6906a97e2c26b116562118b0593457&format=json'
+  const URL = baseurl+keyword+api
+
+  axios.get(URL)
+  .then(function (response) {
+    console.log(URL)
+  })
+
 }
 
 search.addEventListener('click', fetchAlbums)
 
-const url = '/2.0/?method=album.search&album=believe&api_key=529ad6960af792a92fd29962a4cdbb6d&format=json'
 
+
+//ca6906a97e2c26b116562118b0593457
