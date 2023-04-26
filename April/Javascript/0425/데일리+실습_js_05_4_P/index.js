ab6bb727654b4cb8c3992006a8158eadb7192e6f
fpqaut <c6731105@gmail.com>
2023-04-26 16:49:31 +0900

    0426  js 음악 리스트

--- April/Javascript/0425/데일리+실습_js_05_4_P/index.js
+++ April/Javascript/0425/데일리+실습_js_05_4_P/index.js
@@ -10,7 +10,6 @@ function fetchAlbums(page, limit) {
   limit = 10
   // alert('확인!')
   const keyword = document.getElementsByClassName('search-box__input')[0].value
-  console.log(keyword)
 
   const baseurl = 'https://ws.audioscrobbler.com/2.0/?method=album.search&album='
   const api ='&api_key=ca6906a97e2c26b116562118b0593457&format=json'
@@ -19,7 +18,45 @@ function fetchAlbums(page, limit) {
   axios.get(URL)
   .then(function (response) {
     console.log(URL)
+
+    const albums = response.data.results.albummatches.album
+    
+    const search_result = document.querySelector('.search-result')
+
+    for (album in albums) {
+      console.log(album)
+
+      const search_result_card = document.createElement('div')
+      search_result_card.classList.add('search-result__card')
+
+      const image = document.createElement('img')
+      image.src = albums[album].image[1]['#text']
+      search_result_card.append(image)
+
+      const text = document.createElement('div')
+      text.classList.add('search-result__text')
+
+      const name = document.createElement('h2')
+      name.innerText = albums[album].artist
+      text.append(name)
+
+      const title = document.createElement('p')
+      title.innerText = albums[album].name
+      text.append(title)
+
+      search_result_card.append(text)
+      search_result.append(search_result_card)
+
+
+    }
   })
+  .catch((response) => {
+    alert('잠시 후 다시 시도해주세요')
+  })
+
+  
+
+  
 
 }
 
